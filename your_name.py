318b18cf84ae4b8b02bcde062b1de7a9863d57a4
Valor-gt <tybolt6@gmail.com>
2024-01-08 14:51:05 -0500

    20240104 my first commit

---
+++ your_name.py
@@ -0,0 +1,10 @@
+def main():
+    name = "Professor Nedd"
+    print("What is your name?")
+    answer=input()
+    answer = input("Do you like programming "+ name + "?")
+    print("Great! You said " + answer +"?")
+    print("Let's start learning some Python today")
+
+if __name__ == "__main__":
+    main()
