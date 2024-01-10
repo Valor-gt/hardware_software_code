69ad0543b9f54a258ad9c17c6fff7edc93cfef86
Valor-gt <tybolt6@gmail.com>
2024-01-10 13:33:35 -0500

    20240110 H+S class

--- your_file_prof_nedd.py
+++ your_file_prof_nedd.py
@@ -1,10 +1,9 @@
-def main():
+def axel():
     name = input("What is your name?")
     print("We want to know if you like programming!")
-    print()
     answer = input("Do you like programming "+ name + "?")
     print('Great! You said "' + answer + '"?')
     print("Let's learn some Python today")
 
 if __name__ == "__main__":
-    main()
+    axel()
