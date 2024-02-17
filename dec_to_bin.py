04552805f4d94f9e38d250ab0475e67c51db42af
Valor-gt <tybolt6@gmail.com>
2024-02-16 23:20:22 -0500

    H&S Classwork

--- dec_to_bin.py
+++ dec_to_bin.py
@@ -1,12 +1,12 @@
 def decimal_to_binary(number):
     dec_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    solve = number
     for number in number:
         if number.upper() not in dec_list: #check if the number is not in the dec list
             print("Not a decimal number")
 
         elif solve():
-
-def solve():
+            def solve():
                 number = int(number)
                 while number  > 0:            #keep dividing until 0
                     remainder = number % 2
